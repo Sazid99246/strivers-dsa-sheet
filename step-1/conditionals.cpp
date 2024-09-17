6a568a0695da465cc2c8ac036c90b13537d62f5a
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-09-17 22:23:25 +0600

    step-1.3 completed

---
+++ step-1/conditionals.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // write a program that takes an input as an integer
+    // and prints if you are adult or not
+    int age;
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "Yes, you are an adult!" << endl;
+    }
+    else {
+        cout << "You are not an adult!" << endl;
+    }
+    return 0;
+}
